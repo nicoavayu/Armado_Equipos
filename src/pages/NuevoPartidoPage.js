59a43ecd25fc9e61650039f4154a637df62ff60b
Nicolas Avayu <nicoavayu@Mac.fibertel.com.ar>
2026-03-05 10:40:24 -0300

    fix: remove nuevo partido entry transition

--- src/pages/NuevoPartidoPage.js
+++ src/pages/NuevoPartidoPage.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { useAnimatedNavigation } from '../hooks/useAnimatedNavigation';
-import PageTransition from '../components/PageTransition';
 import FormularioNuevoPartidoFlow from './FormularioNuevoPartidoFlow';
 
 const NuevoPartidoPage = () => {
   const { navigateWithAnimation } = useAnimatedNavigation();
   return (
-    <PageTransition>
-      <FormularioNuevoPartidoFlow
-        onConfirmar={async (partido) => {
-          console.log('Match created:', partido.id);
-          navigateWithAnimation(`/admin/${partido.id}`);
-          return partido;
-        }}
-        onVolver={() => navigateWithAnimation('/', 'back')}
-      />
-    </PageTransition>
+    <FormularioNuevoPartidoFlow
+      onConfirmar={async (partido) => {
+        console.log('Match created:', partido.id);
+        navigateWithAnimation(`/admin/${partido.id}`);
+        return partido;
+      }}
+      onVolver={() => navigateWithAnimation('/', 'back')}
+    />
   );
 };
 
